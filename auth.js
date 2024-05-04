d562b407192ae0426013f20536e9dc6d9576f901
AuieGames <auiegames79@gmail.com>
2024-05-03 21:39:01 -0400

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,10 +1,10 @@
-const auth = "twie1e2r9sbn0ilti19y9nhzxawv74"
+const auth = "7ntqm3vbh7fg7mtt9novqswa69xj18"
 	// Authentication and channels - required
 	const channel = "AuieGames"; // your channel
 	const username = "HaniNyuki"; // bot account or your channel
 
 	// You may or may not include the "oauth:" portion of your token
-	const oauth = "twie1e2r9sbn0ilti19y9nhzxawv74";
+	const oauth = "7ntqm3vbh7fg7mtt9novqswa69xj18";
 
 	return {
 		channel,
